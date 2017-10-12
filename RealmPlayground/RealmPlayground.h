c220e7e0649e8a82fc8c68cce4915833a403fe3f
Varun Kochar <varunkochar08@gmail.com>
2017-10-12 08:41:46 -0700

    Initial Commit

---
+++ RealmPlayground/RealmPlayground.h
@@ -0,0 +1,19 @@
+//
+//  RealmPlayground.h
+//  RealmPlayground
+//
+//  Created by Varun on 10/12/17.
+//  Copyright © 2017 Varun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmPlayground.
+FOUNDATION_EXPORT double RealmPlaygroundVersionNumber;
+
+//! Project version string for RealmPlayground.
+FOUNDATION_EXPORT const unsigned char RealmPlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmPlayground/PublicHeader.h>
+
+
